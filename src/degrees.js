a06c25654fed3d72ec7440d061b8d3fa736877c0
pipate14 <62923384+pipate14@users.noreply.github.com>
2021-12-06 21:58:39 -0500

    Update degrees.js

--- src/degrees.js
+++ src/degrees.js
@@ -5,7 +5,7 @@ async function getDegrees(url) {
       .then((response) => response.json())
       .then((data) =>
         document.write(
-          `My first degree was a ${data[0].data.Degree} from ${data[0].data.School} in ${data[0].data["Year Conferred"]}`
+          `My first degree was a ${data.data[0].data.Degree} from ${data.data[0].data.School} in ${data.data[0].data["Year Conferred"]}`
         )
       );
   }
